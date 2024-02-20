678176478d1fb13581acfb2a314f256f3caa7ac0
sarojd799 <saroj.dash799@gmail.com>
2024-02-21 00:53:22 +0530

    delete complete

--- practice/2-row-container/app.js
+++ practice/2-row-container/app.js
@@ -6,61 +6,53 @@ app.directive('scrollableParentContainer', function () {
     return {
         restrict: 'A',
         scope: {
-            data: '='
+            data: '=',
+            onDelete: '&deleteRecord'
         },
         link: function (scope, el, attrs) {
             const containerHeight = 100;
             const containerWidth = 1200;
-            
+
             scope.createParent = function (container, children) {
                 const parent = document.createElement('div')
                 parent.classList.add('alterable-parent')
                 parent.style.height = containerHeight;
                 parent.style.width = containerWidth;
                 container.appendChild(parent);
-                if (children.length > 1) {
-                    container.style.width += 1200;
-                }
+                if (children.length > 1) container.style.width += 1200;
                 return parent;
             }
 
-            scope.deleteData = function(rec) {
-                 scope.data.splice(scope.data.indexOf(rec), 1)
-                 alert('called')
-            }
-
-
-
             scope.$watch('data', function (data, oldData) {
                 if (data.length) {
-                    const action = (data.length > oldData.length || data.length == oldData.length)? 'add': 'remove';
-                    console.log(action)
+                    const action = (data.length > oldData.length || data.length == oldData.length) ? 'add' : 'remove';
                     const sections = Array.from(document.querySelectorAll('.scrollable__parent .alterable-parent'))
                     const container = document.querySelector('.scrollable__parent')
 
-                    let parent;
-                    if (sections.length == 0) {
-                        parent = scope.createParent(container, sections)
-                    } else {
-                        parent = sections[sections.length - 1];
-                    }
+                    let parent = (sections.length == 0) ? scope.createParent(container, sections) : sections[sections.length - 1];
 
                     let records = []
                     if (action == 'add') {
-                      const diff = data.length - oldData.length;
-                      records = data.slice(-diff)
+                        const diff = data.length - oldData.length;
+                        records = data.slice(-diff)
                     } else {
-
+                        records = data;
+                        container.innerHTML = ''
+                        parent = scope.createParent(container, [])
                     }
 
                     for (let rec of records) {
-                        const data = `<div class="drug" ng-click="deleteData(rec)">${rec}</div>`
-                        parent.innerHTML += data;
+                        const child = document.createElement('div');
+                        child.classList.add('drug')
+                        child.textContent = rec;
+                        parent.appendChild(child);
+
                         if (parent.scrollHeight > parent.clientHeight) {
-                            parent.innerHTML = parent.innerHTML.replace(data, '');
+                            parent.removeChild(child)
                             parent = scope.createParent(container, sections)
-                            parent.innerHTML += data;
+                            parent.appendChild(child)
                         }
+                        child.onclick = () => angular.element(document.querySelector('.page-header')).scope().onDelete(rec)
                     }
                 } else {
                     document.querySelector('.scrollable__parent').innerHTML = ''
@@ -149,13 +141,17 @@ app.controller('scrollController', function ($scope) {
         } else if ($scope.savedDrugs.length != 64) {
             $scope.savedDrugs = [...$scope.savedDrugs, $scope.drugs[$scope.savedDrugs.length]]
         }
-        console.log('added')
     }
 
     $scope.assign = function () {
         $scope.savedDrugs = $scope.drugs;
     }
 
-    $scope.assign();
+    //$scope.assign();
+
+    $scope.onDelete = function (match) {
+        $scope.savedDrugs = $scope.savedDrugs.filter(rec => rec !== match)
+        $scope.$apply()
+    }
 
 })
